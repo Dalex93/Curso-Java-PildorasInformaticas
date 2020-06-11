abf98b92401b72b5fb3e3c56b6524f26261a0ace
Dalex93 <54257110+Dalex93@users.noreply.github.com>
2020-06-11 14:38:17 -0600

    Declaración de variables

---
+++ PrimerosPasos/src/Variables.java
@@ -0,0 +1,13 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int edad=35;
+		System.out.println(edad);
+		edad = 75;
+		System.out.println(edad);
+	}
+
+}
